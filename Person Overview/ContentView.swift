7a56d6e89d90639cfb51b7a5656060348d4b37e9
Jan Hovland <jan.hovland@lyse-net>
2019-11-23 11:56:46 +0100

    Update

--- Person Overview/ContentView.swift
+++ Person Overview/ContentView.swift
@@ -5,7 +5,71 @@
 //  Created by Jan Hovland on 23/11/2019.
 //  Copyright © 2019 Jan Hovland. All rights reserved.
 //
+//
+//
+// https://www.youtube.com/watch?v=S41g3E6tkbQ
+// Email Authentication in SwiftUI - Email Login In SwiftUI - Firebase Email Authentication in SwiftUI
+// Bundle identier for the project is: com.janhovland.Person-Overview (- is a space)
+// Bundle identier in Firebase: Person-Overview to register the app
+//
+// Download GoogleService-Info.plist
+// Copy that to the project
+//
+// Say where Xcode-bate.app or Xcode.app is
+// sudo xcode-select --switch /Applications/Xcode-beta.app/Contents/Developer
+//
+// pod init Generates "Podfile"
+//
+/*
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.0'
 
+target 'signupfirebase' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Person Overview
+  
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+  pod 'Firebase/Storage'
+
+end
+*/
+
+/*
+pod install
+
+Analyzing dependencies
+Adding spec repo `trunk` with CDN `https://cdn.cocoapods.org/`
+Downloading dependencies
+Installing Firebase (6.13.0)
+Installing FirebaseAnalytics (6.1.6)
+Installing FirebaseAuth (6.4.0)
+Installing FirebaseAuthInterop (1.0.0)
+Installing FirebaseCore (6.4.0)
+Installing FirebaseCoreDiagnostics (1.1.2)
+Installing FirebaseCoreDiagnosticsInterop (1.1.0)
+Installing FirebaseDatabase (6.1.2)
+Installing FirebaseInstanceID (4.2.7)
+Installing FirebaseStorage (3.4.2)
+Installing GTMSessionFetcher (1.3.0)
+Installing GoogleAppMeasurement (6.1.6)
+Installing GoogleDataTransport (3.2.0)
+Installing GoogleDataTransportCCTSupport (1.2.2)
+Installing GoogleUtilities (6.3.2)
+Installing leveldb-library (1.22)
+Installing nanopb (0.3.9011)
+Generating Pods project
+Integrating client project
+
+[!] Please close any current Xcode sessions and use `Person Overview.xcworkspace` for this project from now on.
+Pod installation complete! There are 4 dependencies from the Podfile and 17 total pods installed.
+
+[!] Automatically assigning platform `iOS` with version `13.2` on target `Person Overview` because no platform was specified. Please specify a platform for this target in your Podfile. See `https://guides.cocoapods.org/syntax/podfile.html#platform`.
+
+*/
 import SwiftUI
 
 struct ContentView: View {
